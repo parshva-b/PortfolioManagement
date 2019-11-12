09491fc4b3367cc7e8226889ae3eac1ee1b8bad7
sangramdesai123 <sangram.desai@somaiya.edu>
2019-11-12 11:41:03 +0530

    stock page design

--- Stockwindow.Designer.cs
+++ Stockwindow.Designer.cs
@@ -28,33 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.l_uid = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.l_email = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.l_money = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // panel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(319, 73);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "This is The Stock Window";
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.l_money);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.l_uid);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.l_email);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.username);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(36, 82);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(261, 319);
+            this.panel1.TabIndex = 3;
+            // 
+            // l_uid
+            // 
+            this.l_uid.AutoSize = true;
+            this.l_uid.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_uid.Location = new System.Drawing.Point(98, 210);
+            this.l_uid.Name = "l_uid";
+            this.l_uid.Size = new System.Drawing.Size(0, 18);
+            this.l_uid.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "User Id  :";
+            // 
+            // l_email
+            // 
+            this.l_email.AutoSize = true;
+            this.l_email.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_email.Location = new System.Drawing.Point(98, 171);
+            this.l_email.Name = "l_email";
+            this.l_email.Size = new System.Drawing.Size(0, 18);
+            this.l_email.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Email    :";
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Font = new System.Drawing.Font("Century725 Cn BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(85, 121);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(90, 25);
+            this.username.TabIndex = 5;
+            this.username.Text = "My Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.ImageLocation = "C:\\Users\\Sangram\\source\\repos\\PortfolioManagement\\img\\1.png";
+            this.pictureBox1.Location = new System.Drawing.Point(73, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // l_money
+            // 
+            this.l_money.AutoSize = true;
+            this.l_money.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_money.Location = new System.Drawing.Point(98, 249);
+            this.l_money.Name = "l_money";
+            this.l_money.Size = new System.Drawing.Size(0, 18);
+            this.l_money.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(26, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 18);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Amount  :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Century725 Cn BT", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.label2.Location = new System.Drawing.Point(333, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 37);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Stock Details";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(333, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(429, 319);
+            this.dataGridView1.TabIndex = 13;
             // 
             // Stockwindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GreenYellow;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
             this.Name = "Stockwindow";
             this.Text = "Stockwindow";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label l_uid;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label l_email;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label l_money;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
